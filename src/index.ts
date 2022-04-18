8026c4a70424a54ae3ed6888d35c3cd57aedd930
Josh Merlino <joshua.merlino@gmail.com>
2022-04-18 07:43:05 -0400

    fuck

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
 /* eslint-disable no-empty */
 // Configure variables in environment file (.env)
 import dotenv from "dotenv";
-import resourcepacks from "./resourcepacks";
 dotenv.config();
-
-const VERSIONS = [ "1.18", "1.17", "1.16", "1.15", "1.14" ];
-
-(async function run(){
-	// *********************
-	// *** RESOURCEPACKS ***
-	// *********************
-	await resourcepacks(VERSIONS);
-}());
--- src/util.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Times out a request
- * @param {number} timeout
- * @return {=> new Promise<{ status: "timeout" }>(resolve => setTimeout(() => resolve(}
- */
-export async function timeout(timeout: number): Promise<{ status: "timeout"; }> {
-	return new Promise<{ status: "timeout" }>(resolve => setTimeout(() => resolve({ status: "timeout" }), timeout));
-}
